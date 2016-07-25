1e95444fe4c880c2a94a17a917f93979a0032b5d
Bhargav Krishna <bhargav.krishna@outlook.com>
2016-07-25 01:38:54 -0700

    adding addin project and moving global styles from app to global.scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {LocationStrategy, HashLocationStrategy} from '@angular/common';
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {ROUTER_DIRECTIVES} from '@angular/router';
 import {APP_ROUTER_PROVIDERS} from "./app.routes";
@@ -6,10 +7,12 @@ import {APP_ROUTER_PROVIDERS} from "./app.routes";
 @Component({
     selector: 'app',
     templateUrl: 'app.component.html',
-    styleUrls: ['app.component.scss'],
     directives: [ROUTER_DIRECTIVES]
 })
 
 export class AppComponent { }
 
-bootstrap(AppComponent, [APP_ROUTER_PROVIDERS]);
+bootstrap(AppComponent, [
+    APP_ROUTER_PROVIDERS,
+    {provide: LocationStrategy, useClass: HashLocationStrategy}
+]);
