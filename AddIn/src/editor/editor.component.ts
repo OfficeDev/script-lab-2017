523992cc6b20bbaf9f48cb5ca66e41b711792810
Bhargav Krishna <bhargav.krishna@outlook.com>
2016-07-27 20:32:30 -0700

    commenting out harcoded snippet values

--- AddIn/src/editor/editor.component.ts
+++ AddIn/src/editor/editor.component.ts
@@ -33,13 +33,12 @@ export class EditorComponent extends BaseComponent implements OnInit, OnDestroy 
             this.snippet = this._snippetManager.findByName(snippetName);
         });
 
-        this.snippet = this._createDefaultNewSnippet();
-
         this.markDispose(subscription);
     }
 
     back() {
-        this._location.back();
+        this._location.replaceState('');
+        this._router.navigate(['']);
     }
 
     save() {
@@ -79,93 +78,93 @@ export class EditorComponent extends BaseComponent implements OnInit, OnDestroy 
         });
     }
 
-    private _createDefaultNewSnippet(): Snippet {
-        var meta = {
-            name: 'Unnamed Snippet',
-            id: 'asbsdasds'
-        };
+    // private _createDefaultNewSnippet(): Snippet {
+    //     var meta = {
+    //         name: 'Unnamed Snippet',
+    //         id: 'asbsdasds'
+    //     };
 
-        var ts = Utilities.stripSpaces(`
-            $("#sample-button").click(runSample);
+    //     var ts = Utilities.stripSpaces(`
+    //         $("#sample-button").click(runSample);
 
-            function runSample() {
-                // This sample will color the selected range in yellow, and also display the selection address.
-                Excel.run(function (ctx) {
-                    var range = ctx.workbook.getSelectedRange();
-                    range.load('address');
-                    return ctx.sync()
-                        .then(function () {
-                            showNotification("Range address is", range.address);
-                        });
-                })
-                .catch(handleError);
-            })
+    //         function runSample() {
+    //             // This sample will color the selected range in yellow, and also display the selection address.
+    //             Excel.run(function (ctx) {
+    //                 var range = ctx.workbook.getSelectedRange();
+    //                 range.load('address');
+    //                 return ctx.sync()
+    //                     .then(function () {
+    //                         showNotification("Range address is", range.address);
+    //                     });
+    //             })
+    //             .catch(handleError);
+    //         })
             
-            function handleError(error) {
-                showNotification("Error", error);
-                // Log additional information to the console, if applicable:
-                if (error instanceof OfficeExtension.Error) {
-                    console.log("Debug info: " + JSON.stringify(error.debugInfo));
-                }
-            }
+    //         function handleError(error) {
+    //             showNotification("Error", error);
+    //             // Log additional information to the console, if applicable:
+    //             if (error instanceof OfficeExtension.Error) {
+    //                 console.log("Debug info: " + JSON.stringify(error.debugInfo));
+    //             }
+    //         }
 
-            var notificationPopup = createNotificationPopup(document.getElementById('notification-popup'));
-            function showNotification(header: string, text: string) {
-                notificationPopup.show(header, text);
-            }
-		`);
+    //         var notificationPopup = createNotificationPopup(document.getElementById('notification-popup'));
+    //         function showNotification(header: string, text: string) {
+    //             notificationPopup.show(header, text);
+    //         }
+	// 	`);
 
-        var html = Utilities.stripSpaces(`
-            <div id="content-main">
-                <h1>Sample snippet</h1>
-                <h3>This sample will color the selected range in yellow, and also display the selection address.</h3>
-                <p>Switch to the JS and CSS views to see more</p>
-                <button id="sample-button">Run sample!</button>
-            </div>
+    //     var html = Utilities.stripSpaces(`
+    //         <div id="content-main">
+    //             <h1>Sample snippet</h1>
+    //             <h3>This sample will color the selected range in yellow, and also display the selection address.</h3>
+    //             <p>Switch to the JS and CSS views to see more</p>
+    //             <button id="sample-button">Run sample!</button>
+    //         </div>
 
-            <div id="notification-popup" class="ms-MessageBanner is-hidden">
-                <div class="notification-popup-title ms-fontSize-l"></div>
-                <div class="ms-MessageBanner-content">
-                    <div class="ms-MessageBanner-text">
-                        <div class="ms-MessageBanner-clipper"></div>
-                    </div>
-                    <!--<button class="ms-MessageBanner-expand"> <i class="ms-Icon ms-Icon--chevronsDown"></i> </button>-->
-                </div>
-                <button class="ms-MessageBanner-close"> <i class="ms-Icon ms-Icon--x"></i> </button>
-            </div>
-        `);
+    //         <div id="notification-popup" class="ms-MessageBanner is-hidden">
+    //             <div class="notification-popup-title ms-fontSize-l"></div>
+    //             <div class="ms-MessageBanner-content">
+    //                 <div class="ms-MessageBanner-text">
+    //                     <div class="ms-MessageBanner-clipper"></div>
+    //                 </div>
+    //                 <!--<button class="ms-MessageBanner-expand"> <i class="ms-Icon ms-Icon--chevronsDown"></i> </button>-->
+    //             </div>
+    //             <button class="ms-MessageBanner-close"> <i class="ms-Icon ms-Icon--x"></i> </button>
+    //         </div>
+    //     `);
 
-        var css = Utilities.stripSpaces(`
-        	/* Notification pane customizations, including overwriting some Fabric UI defaults */
+    //     var css = Utilities.stripSpaces(`
+    //     	/* Notification pane customizations, including overwriting some Fabric UI defaults */
 
-            #notification-popup .notification-popup-title {
-                text-align: left;
-                margin: 10px 50px 0 15px;
-            }
-            #notification-popup.ms-MessageBanner {
-                position: absolute;
-                left: 0px;
-                bottom: 0px;
-                text-align: left;
-                height: inherit;
-            }
-            #notification-popup.ms-MessageBanner, #notification-popup .ms-MessageBanner-text {
-                min-width: inherit;
-            }
-            #notification-popup .ms-MessageBanner-text {
-                margin: 0;
-                padding: 18px 15px;
-            }
-        `);
+    //         #notification-popup .notification-popup-title {
+    //             text-align: left;
+    //             margin: 10px 50px 0 15px;
+    //         }
+    //         #notification-popup.ms-MessageBanner {
+    //             position: absolute;
+    //             left: 0px;
+    //             bottom: 0px;
+    //             text-align: left;
+    //             height: inherit;
+    //         }
+    //         #notification-popup.ms-MessageBanner, #notification-popup .ms-MessageBanner-text {
+    //             min-width: inherit;
+    //         }
+    //         #notification-popup .ms-MessageBanner-text {
+    //             margin: 0;
+    //             padding: 18px 15px;
+    //         }
+    //     `);
 
-        var extras = null;
+    //     var extras = null;
 
-        return new Snippet(<ISnippet>{
-            meta: meta,
-            ts: ts,
-            html: html,
-            css: css,
-            extras: extras
-        });
-    }
+    //     return new Snippet(<ISnippet>{
+    //         meta: meta,
+    //         ts: ts,
+    //         html: html,
+    //         css: css,
+    //         extras: extras
+    //     });
+    // }
 }
