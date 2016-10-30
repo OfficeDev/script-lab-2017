27653f1da0a9bb36e7acd0026183a3164ec678c4
Bhargav Krishna <bhargav.krishna@outlook.com>
2016-10-29 17:17:38 -0700

    removing dependency on lodash

---
+++ typings/index.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="globals/applicationinsights-js/index.d.ts" />
+/// <reference path="globals/bootstrap/index.d.ts" />
+/// <reference path="globals/core-js/index.d.ts" />
+/// <reference path="globals/jquery/index.d.ts" />
+/// <reference path="globals/lodash/index.d.ts" />
+/// <reference path="globals/office-js/index.d.ts" />
