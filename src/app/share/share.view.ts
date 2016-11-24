a80f9aa70e99a7b4b9d71921362d7af0d0ebc8c9
Bhargav Krishna <bhargav.krishna@outlook.com>
2016-11-24 14:06:17 -0800

    Using Disposable in Share

--- src/app/share/share.view.ts
+++ src/app/share/share.view.ts
@@ -1,14 +1,12 @@
-import { Component, OnInit, OnDestroy, ViewChild, ElementRef, HostListener } from '@angular/core';
-import { Http } from '@angular/http';
-import { ViewBase } from '../shared/components/base';
-import { Snippet, SnippetStore, Github } from '../shared/services';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Snippet, SnippetStore, Disposable, Github } from '../shared/services';
 import './share.view.scss';
 
 @Component({
-    selector: 'share',
+    selector: 'share-view',
     templateUrl: 'share.view.html'
 })
-export class ShareView extends ViewBase {
+export class ShareView extends Disposable {
     constructor(
         private _snippetManager: SnippetStore,
         private _github: Github
