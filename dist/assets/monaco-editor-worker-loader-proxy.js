94b5a6566b47f0b9745c68c2e83b57e5b265c1c7
Bhargav Krishna <bhkrish@microsoft.com>
2016-12-14 20:33:48 -0800

    adding initial dist build

---
+++ dist/assets/monaco-editor-worker-loader-proxy.js
@@ -0,0 +1,5 @@
+self.MonacoEnvironment = {
+    baseUrl: 'https://addin-playground.azureedge.net/monaco-editor@0.7.0/min/'
+};
+
+importScripts('https://addin-playground.azureedge.net/monaco-editor@0.7.0/min/vs/base/worker/workerMain.js');
