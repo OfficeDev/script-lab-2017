2d8de3f6ba455b32c45119e730a935c37cda6057
Bhargav Krishna <bhkrish@microsoft.com>
2016-12-14 20:58:13 -0800

    rebuilding dist

--- dist/assets/monaco-editor-worker-loader-proxy.js
+++ dist/assets/monaco-editor-worker-loader-proxy.js
@@ -1,5 +1,5 @@
 self.MonacoEnvironment = {
-    baseUrl: 'https://addin-playground.azureedge.net/monaco-editor@0.7.0/min/'
+    baseUrl: 'https://addin-playground.azureedge.net/monaco-editor/min/'
 };
 
-importScripts('https://addin-playground.azureedge.net/monaco-editor@0.7.0/min/vs/base/worker/workerMain.js');
+importScripts('https://addin-playground.azureedge.net/monaco-editor/min/vs/base/worker/workerMain.js');
