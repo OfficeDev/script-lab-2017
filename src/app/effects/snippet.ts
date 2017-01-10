792a47326b0dde0d31a5265d83c87e460cd96565
Bhargav Krishna <bhkrish@microsoft.com>
2017-01-09 18:43:00 -0800

    minor fix to samples location

--- src/app/effects/snippet.ts
+++ src/app/effects/snippet.ts
@@ -182,7 +182,7 @@ export class SnippetEffects {
         .mergeMap(source => {
             if (source === 'LOCAL') {
                 let snippetJsonUrl = `https://raw.githubusercontent.com/WrathOfZombies/samples/deployment/playlists/${Utilities.host.toLowerCase()}.yaml`;
-                return this._request.local<ITemplate[]>(snippetJsonUrl, ResponseTypes.YAML);
+                return this._request.get<ITemplate[]>(snippetJsonUrl, ResponseTypes.YAML);
             }
             else {
                 return this._request.get<ITemplate[]>(source, ResponseTypes.JSON);
