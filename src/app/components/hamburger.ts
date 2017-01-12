763f26126732d1ff3b4bf3a92ec3980b02271013
Bhargav Krishna <bhkrish@microsoft.com>
2017-01-11 16:53:50 -0800

    hamburger icon fix

--- src/app/components/hamburger.ts
+++ src/app/components/hamburger.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs/Observable';
     template: `
     <section class="hamburger-menu ms-u-slideRightIn10" [class.hamburger-menu--shown]="open|async">
         <header class="command__bar">
-            <command [hidden]="!(open|async)" icon="Cancel" (click)="dismiss.emit()"></command>
+            <command [hidden]="!(open|async)" icon="GlobalNavButton" (click)="dismiss.emit()"></command>
             <command class="title" title="EDITOR"></command>
         </header>
         <div class="hamburger-menu__container">
