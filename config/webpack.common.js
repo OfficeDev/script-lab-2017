9e42648c0f068bf7b892efc314489191e8dbbcaa
Bhargav Krishna <bhkrish@microsoft.com>
2017-01-13 15:23:46 -0800

    adding production telemtry config

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -29,12 +29,12 @@ var config = (function (env) {
         },
         prod: {
             client_id: '8d19e9bbcea2a1cee274',
-            instrumentation_key: '8cc167c3-cd53-49f5-8b5c-bf8bccdb8995',
+            instrumentation_key: '8e23f709-f4ee-4b7d-9a47-2d57224885ce',
             token_url: 'https://addin-playground-runner.azurewebsites.net/auth/prod'
         },
         cdn: {
             client_id: 'b05714a22e602446c43a',
-            instrumentation_key: '8cc167c3-cd53-49f5-8b5c-bf8bccdb8995',
+            instrumentation_key: '8e23f709-f4ee-4b7d-9a47-2d57224885ce',
             token_url: 'https://addin-playground-runner.azurewebsites.net/auth/cdn'
         }
     };
