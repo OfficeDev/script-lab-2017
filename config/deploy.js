5629bc2a05e195f0bfc9dccd337da08e9dd927cd
Bhargav Krishna <bhargav.krishna@outlook.com>
2017-02-27 20:00:41 -0800

    updating build process to only build when configuration allows

--- config/deploy.js
+++ config/deploy.js
@@ -4,9 +4,11 @@ let chalk = require('chalk');
 let _ = require('lodash');
 let { build, config } = require('./env.config');
 let git = require('simple-git')();
+let config = require('./webpack.prod');
+let webpack = require('webpack');
+let rimraf = require('rimraf');
 
 let {TRAVIS, TRAVIS_BRANCH, TRAVIS_PULL_REQUEST, TRAVIS_COMMIT_MESSAGE, AZURE_WA_USERNAME, AZURE_WA_SITE, AZURE_WA_PASSWORD } = process.env;
-
 
 /* Check if the code is running inside of travis.ci. If not abort immediately. */
 if (!TRAVIS) {
@@ -55,39 +57,64 @@ if (slot === 'production') {
     slot = 'staging';
 }
 
-let url = 'https://'
-    + AZURE_WA_USERNAME + ':'
-    + AZURE_WA_PASSWORD + '@'
-    + AZURE_WA_SITE + '-'
-    + slot + '.scm.azurewebsites.net:443/'
-    + AZURE_WA_SITE + '.git';
+rimraf('dist');
 
-log('Deploying to ' + AZURE_WA_SITE + '-' + slot);
+const start = Date.now();
+webpack(config, function (err, stats) {
+    stats.chunks = false;
+    stats.hash = true;
+    stats.version = true;
+    stats.modules = true;
 
-try {
-    git.silent(true)
-        .addConfig('user.name', 'Travis CI')
-        .addConfig('user.email', 'travis.ci@microsoft.com')
-        .checkout('HEAD')
-        .add(['.', '-A', '-f'])
-        .reset(['--', 'node_modules/**'])
-        .commit(TRAVIS_COMMIT_MESSAGE, () => log('Pushing deployment... Please wait...'))
-        .push(['-f', '-q', url, 'HEAD:master'], (err) => {
-            if (err) {
-                return exit('An error occurred. Please fix the build and try again.', true);
-            }
-        })
-        .then(() => {
-            console.log(chalk.bold.green('Successfully deployed to https://' + AZURE_WA_SITE + '-' + slot + '.azurewebsites.net'))
-            process.exit(0);
-        });
-}
-catch (error) {
-    exit(error, true);
-}
+    if (err) {
+        console.log(chalk.bold.red(err));
+        process.exit(1);
+    }
 
-function log(message) {
-    console.log(chalk.bold.cyan(message));
+    if (stats.hasErrors()) {
+        let json = stats.toJson();
+        console.log(chalk.bold.red(json.errors));
+        process.exit(1);
+    }
+
+    const end = Date.now();
+    log('\n\nGenerated build #' + config.build.timestamp + ' in ' + (end - start) / 1000 + ' seconds');
+    log(config.build.name + ' - v' + config.build.version + '\n\n', 'magenta');
+
+    let url = 'https://'
+        + AZURE_WA_USERNAME + ':'
+        + AZURE_WA_PASSWORD + '@'
+        + AZURE_WA_SITE + '-'
+        + slot + '.scm.azurewebsites.net:443/'
+        + AZURE_WA_SITE + '.git';
+
+    log('Deploying to ' + AZURE_WA_SITE + '-' + slot);
+
+    try {
+        git.silent(true)
+            .addConfig('user.name', 'Travis CI')
+            .addConfig('user.email', 'travis.ci@microsoft.com')
+            .checkout('HEAD')
+            .add(['.', '-A', '-f'])
+            .reset(['--', 'node_modules/**'])
+            .commit(TRAVIS_COMMIT_MESSAGE, () => log('Pushing deployment... Please wait...'))
+            .push(['-f', '-q', url, 'HEAD:master'], (err) => {
+                if (err) {
+                    return exit('An error occurred. Please fix the build and try again.', true);
+                }
+            })
+            .then(() => {
+                console.log(chalk.bold.green('Successfully deployed to https://' + AZURE_WA_SITE + '-' + slot + '.azurewebsites.net'))
+                process.exit(0);
+            });
+    }
+    catch (error) {
+        exit(error, true);
+    }
+});
+
+function log(message, color) {
+    console.log(chalk.bold[color || 'cyan'](message));
 }
 
 function exit(reason, abort) {
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -7,7 +7,7 @@ var chalk = require('chalk');
 
 const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
 
-var config = webpackMerge(commonConfig, {
+module.exports = webpackMerge(commonConfig, {
     devtool: 'source-map',
 
     output: {
@@ -37,28 +37,4 @@ var config = webpackMerge(commonConfig, {
             })
         })
     ]
-});
-
-
-const start = Date.now();
-webpack(config, function (err, stats) {
-    stats.chunks = false;
-    stats.hash = true;
-    stats.version = true;
-    stats.modules = true;
-
-    if (err) {
-        console.log(chalk.bold.red(err));
-        process.exit(1);
-    }
-
-    if (stats.hasErrors()) {
-        let json = stats.toJson();
-        console.log(chalk.bold.red(json.errors));
-        process.exit(1);
-    }
-
-    const end = Date.now();
-    console.log(chalk.bold.green('\n\nGenerated build #' + config.build.timestamp + ' in ' + (end - start) / 1000 + ' seconds'));
-    console.log(chalk.bold.magenta(config.build.name + ' - v' + config.build.version + '\n\n'));
 });
