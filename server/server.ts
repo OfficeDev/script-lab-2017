eac6f352c6f8014ee5c0ab498ea601069f616b95
Bhargav Krishna <bhargav.krishna@outlook.com>
2017-03-03 10:00:24 -0800

    runner upgrade in progress

--- server/server.ts
+++ server/server.ts
@@ -13,140 +13,125 @@ import { RunnerError } from './core/runner-error';
 import { config } from './core/tokens';
 import * as appInsights from 'applicationinsights';
 
-function Server() {
-    appInsights.setup(config.instrumentation_key).start();
-    let app = express();
+appInsights.setup(config.instrumentation_key).start();
+let app = express();
 
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
-    app.get('/', async (request: express.Request, response: express.Response) => {
-        console.log(request);
-        return new Promise(() => {
-            fs.readFile(path.resolve(`${__dirname}/assets/editor-runner.html`), 'UTF8', (err, data) => {
-                if (err != null) {
-                    return handleError(err, response);
-                } else {
-                    return response.contentType('text/html').status(200).send(data);
-                }
-            });
-        });
-    });
+app.get('/', (req: express.Request, res: express.Response) => res.sendfile(path.resolve('assets/editor-runner.html')));
 
-    app.post('/auth/:env', async (request: express.Request, response: express.Response) => {
-        if (request.body == null || request.body.code == null || request.body.code.trim() === '') {
-            return handleError(new RunnerError('Received invalid code.', request.body), response);
-        }
-
-        try {
-            let env = request.params.env;
-            let {code, state} = request.body;
-            let source = config[env];
-
-            if (source == null) {
-                return handleError(new RunnerError(`Bad environment configuration: ${env}`), response);
-            }
-
-            let {client_id, client_secret, redirect_uri } = source;
-
-            return Request.post({
-                url: 'https://github.com/login/oauth/access_token',
-                headers: {
-                    'Accept': 'application/json'
-                },
-                json: {
-                    client_id,
-                    client_secret,
-                    redirect_uri,
-                    code,
-                    state
-                }
-            }, (error, httpResponse, body) => {
-                if (error) {
-                    return handleError(new RunnerError('Error retrieving GitHub access token', error), response);
-                }
-                else {
-                    return response.contentType('application/json').status(200).send(body);
-                }
-            });
-        }
-        catch (error) {
-            return handleError(error, response);
-        }
-    });
-
-    app.post('/', async (request: express.Request, response: express.Response) => {
-        let returnUrl: string;
-
-        try {
-            // The snippet might come in either wrapped in a serialized "data" object, or as is
-            let data: IRunnerPostData = request.body;
-            if (!data.snippet) {
-                data = JSON.parse(request.body.data);
-            }
-            if (!data.snippet) {
-                throw new RunnerError('Received invalid snippet data.', request.body);
-            }
-
-            returnUrl = data.returnUrl;
-
-            let start = Date.now();
-            let compiledSnippet = await SnippetGenerator.compile(jsyaml.safeLoad(<string>data.snippet));
-            let tsEnd = Date.now();
-
-            TemplateGenerator.initCodeHelpers();
-
-            let html = await TemplateGenerator.generate('inner-template.html', compiledSnippet);
-
-            // If there are additional fields on data, like returnUrl, wrap it in the outer gallery-run template
-            if (data.returnUrl) {
-                let wrapperContext = SnippetTemplateGenerator
-                    .createOuterTemplateContext(html, data, compiledSnippet);
-                html = await TemplateGenerator.generate('outer-template.html', wrapperContext);
-            }
-
-            html = Utilities.replaceAllTabsWithSpaces(html);
-
-            let snippetEnd = Date.now();
-            appInsights.client.trackEvent('[RUNNER] Compilation Complete',
-                {
-                    ID: compiledSnippet.id,
-                },
-                {
-                    SNIPPET_COMPILE: tsEnd - start,
-                    TEMPLATE_COMPILE: snippetEnd - tsEnd,
-                    TOTAL_COMPILE: snippetEnd - start
-                });
-
-            appInsights.client.trackEvent(`[RUNNER] Running ${compiledSnippet.id}`, { ID: compiledSnippet.id });
-            return response.contentType('text/html').status(200).send(html);
-        }
-        catch (error) {
-            return handleError(error, response, returnUrl);
-        };
-    });
-
-    async function handleError(error: Error, response: express.Response, returnUrl?: string) {
-        appInsights.client.trackException(error);
-
-        let context: { message: string, details: string, returnUrl: string } = {
-            message: error.message,
-            details: error instanceof RunnerError ? error.details : jsyaml.safeDump(error),
-            returnUrl: returnUrl
-        }
-
-        let body = await TemplateGenerator.generate('error.html', context);
-        return response.contentType('text/html').status(200).send(body);
+app.post('/auth/:env', async (request: express.Request, response: express.Response) => {
+    if (request.body == null || request.body.code == null || request.body.code.trim() === '') {
+        return handleError(new RunnerError('Received invalid code.', request.body), response);
     }
 
-    return app;
+    try {
+        let env = request.params.env;
+        let { code, state } = request.body;
+        let source = config[env];
+
+        if (source == null) {
+            return handleError(new RunnerError(`Bad environment configuration: ${env}`), response);
+        }
+
+        let { client_id, client_secret, redirect_uri } = source;
+
+        return Request.post({
+            url: 'https://github.com/login/oauth/access_token',
+            headers: {
+                'Accept': 'application/json'
+            },
+            json: {
+                client_id,
+                client_secret,
+                redirect_uri,
+                code,
+                state
+            }
+        }, (error, httpResponse, body) => {
+            if (error) {
+                return handleError(new RunnerError('Error retrieving GitHub access token', error), response);
+            }
+            else {
+                return response.contentType('application/json').status(200).send(body);
+            }
+        });
+    }
+    catch (error) {
+        return handleError(error, response);
+    }
+});
+
+app.post('/', async (request: express.Request, response: express.Response) => {
+    let returnUrl: string;
+
+    try {
+        // The snippet might come in either wrapped in a serialized "data" object, or as is
+        let data: IRunnerPostData = request.body;
+        if (!data.snippet) {
+            data = JSON.parse(request.body.data);
+        }
+        if (!data.snippet) {
+            throw new RunnerError('Received invalid snippet data.', request.body);
+        }
+
+        returnUrl = data.returnUrl;
+
+        let start = Date.now();
+        let compiledSnippet = await SnippetGenerator.compile(jsyaml.safeLoad(<string>data.snippet));
+        let tsEnd = Date.now();
+
+        TemplateGenerator.initCodeHelpers();
+
+        let html = await TemplateGenerator.generate('inner-template.html', compiledSnippet);
+
+        // If there are additional fields on data, like returnUrl, wrap it in the outer gallery-run template
+        if (data.returnUrl) {
+            let wrapperContext = SnippetTemplateGenerator
+                .createOuterTemplateContext(html, data, compiledSnippet);
+            html = await TemplateGenerator.generate('outer-template.html', wrapperContext);
+        }
+
+        html = Utilities.replaceAllTabsWithSpaces(html);
+
+        let snippetEnd = Date.now();
+        appInsights.client.trackEvent('[RUNNER] Compilation Complete',
+            {
+                ID: compiledSnippet.id,
+            },
+            {
+                SNIPPET_COMPILE: tsEnd - start,
+                TEMPLATE_COMPILE: snippetEnd - tsEnd,
+                TOTAL_COMPILE: snippetEnd - start
+            });
+
+        appInsights.client.trackEvent(`[RUNNER] Running ${compiledSnippet.id}`, { ID: compiledSnippet.id });
+        return response.contentType('text/html').status(200).send(html);
+    }
+    catch (error) {
+        return handleError(error, response, returnUrl);
+    };
+});
+
+async function handleError(error: Error, response: express.Response, returnUrl?: string) {
+    appInsights.client.trackException(error);
+
+    let context: { message: string, details: string, returnUrl: string } = {
+        message: error.message,
+        details: error instanceof RunnerError ? error.details : jsyaml.safeDump(error),
+        returnUrl: returnUrl
+    }
+
+    let body = await TemplateGenerator.generate('error.html', context);
+    return response.contentType('text/html').status(200).send(body);
 }
 
-let server = Server();
+
 https.createServer({
     key: fs.readFileSync(path.resolve('node_modules/browser-sync/lib/server/certs/server.key')),
     cert: fs.readFileSync(path.resolve('node_modules/browser-sync/lib/server/certs/server.crt'))
-}, server).listen(process.env.PORT || 3200, () => {
+}, app).listen(process.env.PORT || 3200, () => {
     console.log(`Playground server running on ${process.env.PORT || 3200}`)
 });
