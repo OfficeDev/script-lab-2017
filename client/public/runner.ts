3e4e93979099f7bfb3c64170b8ec6a2b3b3d89e3
Bhargav Krishna <bhargav.krishna@outlook.com>
2017-03-08 14:37:40 -0800

    working runner

--- client/public/runner.ts
+++ client/public/runner.ts
@@ -1,45 +1,47 @@
-import { environment } from '../app/helpers';
+import * as $ from 'jquery';
+import '../assets/styles/runner.scss';
 
-(($) => {
-    function loadFirebug() {
-        return new Promise<boolean>((resolve, reject) => {
-            const script = $(`<script type="text/javascript" src="${environment.current.config.editorUrl}/assets/firebug/firebug-lite-debug.js#startOpened"></script>`);
-            script.appendTo('head');
+function loadFirebug(origin) {
+    return new Promise<boolean>((resolve, reject) => {
+        (window as any).origin = origin;
+        const script = $(`<script type="text/javascript" src="${origin}/assets/firebug/firebug-lite-debug.js#startOpened"></script>`);
+        script.appendTo('head');
 
-            const interval = setInterval(() => {
-                if ((window as any).firebugLiteIsLoaded) {
-                    clearInterval(interval);
-                    return resolve(true);
-                }
-            }, 250);
-        });
+        const interval = setInterval(() => {
+            if ((window as any).firebugLiteIsLoaded) {
+                clearInterval(interval);
+                return resolve(true);
+            }
+        }, 250);
+    });
+}
+
+const initializeRunner = async (origin: string, host: string, platform: string, officeJS: string) => {
+    await loadFirebug(origin);
+
+    let iframe = $('#snippet-container');
+    let content = $('#iframe-code-content');
+    let progress = $('#progress');
+    let header = $('#header');
+
+    let { contentWindow } = iframe[0] as HTMLIFrameElement;
+    (contentWindow as any).console = window.console;
+    contentWindow.onerror = (...args) => console.error(args);
+
+    contentWindow.document.open();
+    contentWindow.document.write(content.text());
+    contentWindow.document.close();
+    content.remove();
+
+    (window as any).Firebug.chrome.open();
+    if (officeJS) {
+        contentWindow['Office'] = officeJS ? window['Office'] : {};
+        ['OfficeExtension', 'Excel', 'Word', 'OneNote'].forEach(namespace => contentWindow[namespace] = window[namespace]);
     }
 
-    const initializeRunner = async (host, platform, officeJS: string) => {
-        await environment.initialize(host, platform);
-        await loadFirebug();
+    iframe.show();
+    progress.hide();
+    header.show();
+};
 
-        let iframe = $('#snippet-container');
-        let content = $('#iframe-content');
-        let { contentWindow } = iframe[0] as HTMLIFrameElement;
-
-        (contentWindow as any).console = window.console;
-        if (officeJS) {
-            contentWindow['Office'] = window['Office'] || {};
-            contentWindow.onerror = (...args) => console.error(args);
-            iframe.contents().find('html').html(content.text());
-            content.remove();
-        }
-
-        (window as any).runnerReady = () => {
-            (window as any).Firebug.chrome.open();
-            if (officeJS) {
-                ['OfficeExtension', 'Excel', 'Word', 'OneNote'].forEach(namespace => contentWindow[namespace] = window[namespace]);
-            }
-            iframe.show();
-            $('#progress').hide();
-        };
-    };
-
-    (window as any).initializeRunner = initializeRunner;
-})(jQuery);
+(window as any).initializeRunner = initializeRunner;
--- server/core/snippet.generator.ts
+++ server/core/snippet.generator.ts
@@ -31,7 +31,8 @@ class SnippetGenerator {
                 this.compileScript(snippet.script)
             ]);
 
-        compiledSnippet.script = script;
+        // HACK: Need to manually remove es2015 module generation
+        compiledSnippet.script = script.replace('Object.defineProperty(exports, "__esModule", { value: true });', '');
         compiledSnippet.officeJS = officeJS;
         compiledSnippet.scriptReferences = scriptReferences;
         compiledSnippet.linkReferences = linkReferences;
@@ -56,7 +57,7 @@ class SnippetGenerator {
             text = text.trim();
 
             let isNotScriptOrStyle =
-                /^\/\/.*|^\/\*.*|.*\*\/$.*/im.test(text) ||
+                /^#.*|^\/\/.*|^\/\*.*|.*\*\/$.*/im.test(text) ||
                 /^@types/.test(text) ||
                 /^dt~/.test(text) ||
                 /\.d\.ts$/i.test(text);
@@ -65,7 +66,7 @@ class SnippetGenerator {
                 return null;
             }
 
-            let resolvedUrlPath = (/^https?:\/\/|^ftp? :\/\//i.test(text)) ? text : `//unpkg.com/${text}`;
+            let resolvedUrlPath = (/^https?:\/\/|^ftp? :\/\//i.test(text)) ? text : `https://unpkg.com/${text}`;
 
             if (/\.css$/i.test(resolvedUrlPath)) {
                 return linkReferences.push(resolvedUrlPath);
@@ -77,7 +78,7 @@ class SnippetGenerator {
                 * it is special because of how it needs to be *outside* of the iframe,
                 * whereas the rest of the script references need to be inside the iframe.
                 */
-                if (/(?:office|office.debug).js$/.test(resolvedUrlPath)) {
+                if (/(?:office|office.debug).js$/.test(resolvedUrlPath.toLowerCase())) {
                     officeJS = resolvedUrlPath;
                     return null;
                 }
@@ -85,7 +86,7 @@ class SnippetGenerator {
                 return scriptReferences.push(resolvedUrlPath);
             }
 
-            return null;
+            return scriptReferences.push(resolvedUrlPath);
         }
     }
 
@@ -99,21 +100,20 @@ class SnippetGenerator {
                     compilerOptions: {
                         target: ts.ScriptTarget.ES5,
                         allowJs: true,
-                        sourceMap: false,
+                        inlineSourceMap: true,
                         lib: ['dom', 'es2015']
                     }
                 });
 
                 if (result.diagnostics.length) {
-                    throw new BadRequestError('Errors during TypeScript compilation',
-                        result.diagnostics.map(item => {
-                            let upThroughError = content.substr(0, item.start);
-                            let afterError = content.substr(item.start + 1);
-                            let lineNumber = upThroughError.split('\n').length;
-                            let startIndexOfThisLine = upThroughError.lastIndexOf('\n');
-                            let lineText = content.substring(startIndexOfThisLine, item.start + Math.max(afterError.indexOf('\n'), 0)).trim();
-                            return `Line #${lineNumber}:  ${item.messageText}` + '\n\n' + lineText;
-                        }).join('\n\n\n')
+                    throw new BadRequestError(result.diagnostics.map(item => {
+                        let upThroughError = content.substr(0, item.start);
+                        let afterError = content.substr(item.start + 1);
+                        let lineNumber = upThroughError.split('\n').length;
+                        let startIndexOfThisLine = upThroughError.lastIndexOf('\n');
+                        let lineText = content.substring(startIndexOfThisLine, item.start + Math.max(afterError.indexOf('\n'), 0)).trim();
+                        return `Line #${lineNumber}:  ${item.messageText}` + '\n\n' + lineText;
+                    }).join('\n\n\n')
                     );
                 }
 
