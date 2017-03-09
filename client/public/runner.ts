e9c477c9c30589939dbeb913400b1ac0ea6b3478
Bhargav Krishna <bhargav.krishna@outlook.com>
2017-03-09 14:22:51 -0800

    fix for runner errors

--- client/public/runner.ts
+++ client/public/runner.ts
@@ -1,9 +1,8 @@
 import * as $ from 'jquery';
 import '../assets/styles/runner.scss';
-declare var Firebug: any;
 
 function loadFirebug(origin) {
-    return new Promise<boolean>((resolve, reject) => {
+    return new Promise<any>((resolve, reject) => {
         (window as any).origin = origin;
         const script = $(`<script type="text/javascript" src="${origin}/assets/firebug/firebug-lite-debug.js#startOpened"></script>`);
         script.appendTo('head');
@@ -11,38 +10,45 @@ function loadFirebug(origin) {
         const interval = setInterval(() => {
             if ((window as any).firebugLiteIsLoaded) {
                 clearInterval(interval);
-                return resolve(true);
+                return resolve((window as any).Firebug);
             }
         }, 250);
     });
 }
 
 const initializeRunner = async (origin: string, host: string, platform: string, officeJS: string) => {
-    await loadFirebug(origin);
+    try {
+        const firebug = await loadFirebug(origin);
 
-    let iframe = $('#snippet-container');
-    let content = $('#iframe-code-content');
-    let progress = $('#progress');
-    let header = $('#header');
+        let iframe = $('#snippet-container');
+        let content = $('#iframe-code-content');
+        let progress = $('#progress');
+        let header = $('#header');
 
-    let { contentWindow } = iframe[0] as HTMLIFrameElement;
-    (contentWindow as any).console = window.console;
-    contentWindow.onerror = (...args) => console.error(args);
+        let { contentWindow } = iframe[0] as HTMLIFrameElement;
+        (contentWindow as any).console = window.console;
+        contentWindow.onerror = (...args) => console.error(args);
 
-    contentWindow.document.open();
-    contentWindow.document.write(content.text());
-    contentWindow.document.close();
-    content.remove();
+        contentWindow.document.open();
+        contentWindow.document.write(content.text());
+        contentWindow.document.close();
+        content.remove();
 
-    if (officeJS) {
-        contentWindow['Office'] = officeJS ? window['Office'] : {};
-        ['OfficeExtension', 'Excel', 'Word', 'OneNote'].forEach(namespace => contentWindow[namespace] = window[namespace]);
+        if (officeJS) {
+            contentWindow['Office'] = officeJS ? window['Office'] : {};
+            ['OfficeExtension', 'Excel', 'Word', 'OneNote'].forEach(namespace => contentWindow[namespace] = window[namespace]);
+        }
+
+        iframe.show();
+        progress.hide();
+        header.show();
+        if (firebug.chrome) {
+            firebug.chrome.open();
+        }
     }
+    catch (e) {
 
-    iframe.show();
-    progress.hide();
-    header.show();
-    Firebug.chrome.open();
+    }
 };
 
 (window as any).initializeRunner = initializeRunner;
