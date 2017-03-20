f71d8964c2d4798c159d5dec43fa01dd4667bdef
Zlatkovsky <michael.zlat@gmail.com>
2017-03-20 14:53:26 -0700

    Add "last updated" to editor gallery as well https://github.com/OfficeDev/addin-playground/issues/219

--- client/app/containers/gallery.list.ts
+++ client/app/containers/gallery.list.ts
@@ -1,5 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { groupBy, isEmpty } from 'lodash';
+import * as moment from 'moment';
 
 @Component({
     selector: 'gallery-list',
@@ -8,7 +9,7 @@ import { groupBy, isEmpty } from 'lodash';
             <section class="gallery-list__group" *ngFor="let group of (groupedItems|keys)">
                 <collapse [title]="group?.key">
                     <section class="gallery-list__group">
-                        <article class="gallery-list__item gallery-list__item--template ms-font-m" *ngFor="let item of group?.value" (click)="select.emit(item)" [ngClass]="{'gallery-list__item--selected' : item?.id === current?.id}">
+                        <article class="gallery-list__item gallery-list__item--template ms-font-m" *ngFor="let item of group?.value" (mouseover)="setLastUpdatedText(item)" [title]="item.lastUpdatedText" (click)="select.emit(item)" [ngClass]="{'gallery-list__item--selected' : item?.id === current?.id}">
                             <div class="name">{{item?.name}}</div>
                             <div class="description">{{item?.description}}</div>
                         </article>
@@ -35,4 +36,22 @@ export class GalleryList {
             this.empty = isEmpty(this.groupedItems);
         }
     }
+
+    setLastUpdatedText(item: ISnippet): void {
+        if (item.modified_at) {
+            (item as any).lastUpdatedText = `Last updated ${moment(item.modified_at).fromNow()}`;
+        }
+    }
+
+    static setUpMomentJsDurationDefaults() {
+        moment.relativeTimeThreshold('s', 40);
+        // Note, per documentation, "ss" must be set after "s"
+        moment.relativeTimeThreshold('ss', 2);
+        moment.relativeTimeThreshold('m', 40);
+        moment.relativeTimeThreshold('h', 20);
+        moment.relativeTimeThreshold('d', 25);
+        moment.relativeTimeThreshold('M', 10);
+    }
 };
+
+GalleryList.setUpMomentJsDurationDefaults();
