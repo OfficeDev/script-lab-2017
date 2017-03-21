c086bb40399cdb42ad1dd425ab98f65cb40ba99c
Bhargav Krishna <bhargav.krishna@outlook.com>
2017-03-20 23:56:38 -0700

    adding support for cli env params

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -3,52 +3,53 @@ const webpackMerge = require('webpack-merge');
 const commonConfig = require('./webpack.common.js');
 const path = require('path');
 
-module.exports = webpackMerge(commonConfig, {
-    devtool: '#source-map',
+module.exports = (env) => 
+    webpackMerge(commonConfig(env.mode === 'prod', {
+        devtool: '#source-map',
 
-    output: {
-        path: path.resolve('./dist/client'),
-        filename: '[name].bundle.js',
-        chunkFilename: '[name].chunk.js',
-    },
-
-    resolve: {
-        modules: ["node_modules"]
-    },
-
-    devServer: {
-        publicPath: "/",
-        contentBase: path.resolve('./dist/client'),
-        https: true,
-        overlay: {
-            warnings: false,
-            errors: true
+        output: {
+            path: path.resolve('./dist/client'),
+            filename: '[name].bundle.js',
+            chunkFilename: '[name].chunk.js',
         },
-        open: true,
-        port: 3000,
-        hot: true,
-        quiet: true,
-        historyApiFallback: true,
-        stats: {
-            assets: false,
-            cached: false,
-            children: false,
-            chunks: true,
-            chunkModules: true,
-            chunkOrigins: false,
-            context: "./dist/client/",
-            colors: true,
-            errors: true,
-            errorDetails: true,
-            hash: true,
-            modules: false,
-            modulesSort: "field",
-            publicPath: true,
-            reasons: false,
-            source: false,
-            timings: true,
-            version: true,
-            warnings: true
+
+        resolve: {
+            modules: ["node_modules"]
+        },
+
+        devServer: {
+            publicPath: "/",
+            contentBase: path.resolve('./dist/client'),
+            https: true,
+            overlay: {
+                warnings: false,
+                errors: true
+            },
+            open: true,
+            port: 3000,
+            hot: true,
+            quiet: true,
+            historyApiFallback: true,
+            stats: {
+                assets: false,
+                cached: false,
+                children: false,
+                chunks: true,
+                chunkModules: true,
+                chunkOrigins: false,
+                context: "./dist/client/",
+                colors: true,
+                errors: true,
+                errorDetails: true,
+                hash: true,
+                modules: false,
+                modulesSort: "field",
+                publicPath: true,
+                reasons: false,
+                source: false,
+                timings: true,
+                version: true,
+                warnings: true
+            }
         }
-    }
-});
+    });
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -5,54 +5,55 @@ const path = require('path');
 const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
 const { TRAVIS } = process.env;
 
-module.exports = webpackMerge(commonConfig, {
-    devtool: 'source-map',
+module.exports = (env) => 
+    webpackMerge(commonConfig(env.mode === 'prod'), {
+        devtool: 'source-map',
 
-    output: {
-        path: './dist/client',
-        filename: '[name].bundle.js',
-        chunkFilename: '[id].chunk.js'
-    },
+        output: {
+            path: './dist/client',
+            filename: '[name].bundle.js',
+            chunkFilename: '[id].chunk.js'
+        },
 
-    performance: {
-        hints: "warning"
-    },
+        performance: {
+            hints: "warning"
+        },
 
-    stats: {
-        assets: true,
-        cached: false,
-        children: false,
-        chunks: true,
-        chunkModules: true,
-        chunkOrigins: false,
-        context: "./dist/client/",
-        colors: true,
-        errors: true,
-        errorDetails: true,
-        hash: true,
-        modules: false,
-        modulesSort: "field",
-        publicPath: true,
-        reasons: false,
-        source: false,
-        timings: true,
-        version: true,
-        warnings: false
-    },
+        stats: {
+            assets: true,
+            cached: false,
+            children: false,
+            chunks: true,
+            chunkModules: true,
+            chunkOrigins: false,
+            context: "./dist/client/",
+            colors: true,
+            errors: true,
+            errorDetails: true,
+            hash: true,
+            modules: false,
+            modulesSort: "field",
+            publicPath: true,
+            reasons: false,
+            source: false,
+            timings: true,
+            version: true,
+            warnings: false
+        },
 
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            sourceMap: true,
-            compress: true,
-            mangle: {
-                screw_ie8: true,
-                keep_fnames: true
-            },
-            compress: {
-                warnings: false,
-                screw_ie8: true
-            },
-            comments: false
-        })        
-    ]
-});
+        plugins: [
+            new webpack.optimize.UglifyJsPlugin({
+                sourceMap: true,
+                compress: true,
+                mangle: {
+                    screw_ie8: true,
+                    keep_fnames: true
+                },
+                compress: {
+                    warnings: false,
+                    screw_ie8: true
+                },
+                comments: false
+            })        
+        ]
+    });
