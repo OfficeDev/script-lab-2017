5f7752f9c433230cfea3672ab08cdd1fe1c2e367
Bhargav Krishna <bhargav.krishna@outlook.com>
2017-03-21 19:09:21 -0700

    removing unsused vars

--- src/client/app/components/profile.ts
+++ src/client/app/components/profile.ts
@@ -1,5 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { Strings, settings } from '../helpers';
+import { Strings } from '../helpers';
 
 @Component({
     selector: 'profile',
