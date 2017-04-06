cebf995978b62c6b440a91263315494437779a51
Bhargav Krishna <bhargav.krishna@outlook.com>
2017-04-06 12:27:48 -0700

    updating about component

--- src/client/app/components/about.ts
+++ src/client/app/components/about.ts
@@ -1,5 +1,5 @@
 import { Component, Input, ChangeDetectionStrategy, Output, EventEmitter } from '@angular/core';
-import { environment, storageSize, Strings } from '../helpers';
+import { environment, storageSize, Strings, storage } from '../helpers';
 
 @Component({
     changeDetection: ChangeDetectionStrategy.OnPush,
@@ -10,7 +10,8 @@ import { environment, storageSize, Strings } from '../helpers';
                 <div class="about__image"></div>
                 <div class="about__details">
                     <div class="about__primary-text ms-font-xxl">{{config?.name}}</div>
-                    <div class="about__secondary-text ms-font-l">Version: {{config?.version}}</div>
+                    <div class="profile__tertiary-text ms-font-m">ID: ${storage.user}</div>
+                    <div class="about__secondary-text ms-font-l">Version: {{config?.version}} ({{config?.timestamp}})</div>
                     <pre class="about__tertiary-text ms-font-m">{{cache}}</pre>
                 </div>
             </div>
@@ -31,9 +32,6 @@ export class About {
     config = environment.current.build;
 
     cache = `
-    ${Strings.aboutUpdated}
-    ${new Date(environment.current.build.timestamp)}
-
     ${Strings.aboutStorage}
     ${storageSize(localStorage, `playground_${environment.current.host}_snippets`, Strings.aboutSnippets)}
     ${storageSize(sessionStorage, 'playground_intellisense', Strings.aboutIntellisense)}
