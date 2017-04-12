f43d798eefb2b7bf4ce4e3f33a3275c16b04dce2
saunders77 <michaelsaunders@outlook.com>
2017-04-11 17:56:14 -0700

    reverting for now

--- src/client/public/functions.ts
+++ src/client/public/functions.ts
@@ -35,13 +35,20 @@ Office.initialize = () => {
         }
     };
 
-    (window as any).launchTutorial = (event) => launchFromDialog(urls.tutorial, event);
+    const launchInDialog = (url: string, event?: any) => {
+        Office.context.ui.displayDialogAsync(url,null , (asyncResult) => {});
+        if (event) {
+            event.completed();
+        }
+    };
 
-    (window as any).launchHelp = (event) => launchFromDialog(urls.playground_help, event);
+    (window as any).launchTutorial = (event) => launch(urls.tutorial, event);
 
-    (window as any).launchFeedback = (event) => launchFromDialog(urls.feedback, event);
+    (window as any).launchHelp = (event) => launch(urls.playground_help, event);
 
-    (window as any).launchAsk = (event) => launchFromDialog(urls.ask, event);
+    (window as any).launchFeedback = (event) => launch(urls.feedback, event);
+
+    (window as any).launchAsk = (event) => launch(urls.ask, event);
 
     (window as any).launchApiDocs = (event) => {
         if (Office.context.requirements.isSetSupported('ExcelApi')) {
