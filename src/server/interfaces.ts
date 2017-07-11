245468fc042e467b9f7c4b5db5f69f995315e5ca
Shashwat Chaturvedi <t-shac@microsoft.com>
2017-07-11 08:10:42 -0700

    Fix interface field

--- src/server/interfaces.ts
+++ src/server/interfaces.ts
@@ -20,7 +20,7 @@ export interface IRunnerHandlebarsContext {
 
     origin: string;
     host: string;
-    assets: { [key: string]: string };
+    assets: { [key: string]: any };
 
     initialLoadSubtitle: string;
     headerTitle: string;
@@ -37,7 +37,7 @@ export interface IRunnerHandlebarsContext {
 
 export interface IErrorHandlebarsContext {
     origin: string;
-    assets: { [key: string]: string };
+    assets: { [key: string]: any };
     title: string;
     message: string;
     details: string;
