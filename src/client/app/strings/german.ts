2c37b3669a8494482d3694afa3cafdc9bde20f16
Mourad Louha <git@maninweb.de>
2017-07-12 17:57:14 +0200

    Update for the translation to German (#493)

--- src/client/app/strings/german.ts
+++ src/client/app/strings/german.ts
@@ -4,8 +4,8 @@ import { ClientStrings } from './index';
 // Whenever these two lines are not commented out, it means that there are
 // still strings that need to be localized.
 // Just search for "englishSubstitutesForNotYetTranslated" in this file
-import { getEnglishStrings } from './english';
-const englishSubstitutesForNotYetTranslated = getEnglishStrings();
+// import { getEnglishStrings } from './english';
+// const englishSubstitutesForNotYetTranslated = getEnglishStrings();
 
 export function getGermanStrings(): ClientStrings {
     const playgroundName = 'Script Lab';
@@ -87,7 +87,7 @@ export function getGermanStrings(): ClientStrings {
         snippetCopiedFailed: 'Das Schnipsel konnte nicht in die Zwischenablage kopiert werden.',
 
         snippetExportFailed: 'Das Schnipsel konnte nicht exportiert werden.',
-        snippetExportNotSupported: englishSubstitutesForNotYetTranslated.snippetExportNotSupported,
+        snippetExportNotSupported: 'Das Exportieren von Schnipseln wird in dieser Office-Version noch nicht unterstützt. Unterstützte Plattformen sind zurzeit Windows und Office-Online.',
 
         // Components strings
         // about.ts
@@ -95,9 +95,9 @@ export function getGermanStrings(): ClientStrings {
         aboutStorage: 'Speicher:',
         aboutSnippets: 'Lokale Schnipsel',
         aboutIntellisense: 'IntelliSense',
-        aboutCurrentEnvironment: englishSubstitutesForNotYetTranslated.aboutCurrentEnvironment,
-        aboutSwitchEnvironment: englishSubstitutesForNotYetTranslated.aboutSwitchEnvironment,
-        changeEnvironmentConfirm: englishSubstitutesForNotYetTranslated.changeEnvironmentConfirm,
+        aboutCurrentEnvironment: 'Aktuelle Umgebung:',
+        aboutSwitchEnvironment: 'Wechseln:',
+        changeEnvironmentConfirm: 'Sie sind dabei, zu einer anderen Umgebung von Script Lab zu wechseln und verlieren dadurch den Zugriff auf Ihre gespeicherten Schnipsel bis Sie wieder zu dieser Umgebung zurückkehren. Möchten Sie fortfahren?',
 
         //snippet.info.ts
         snippetInfoDialogTitle: 'Schnipsel-Info',
