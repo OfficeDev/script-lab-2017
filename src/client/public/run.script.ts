9bad707bef3be757323a2a189ff0b44b11eebd81
Shashwat Chaturvedi <t-shac@microsoft.com>
2017-07-12 17:17:24 -0700

    Ribbon now only appears on right side

--- src/client/public/run.script.ts
+++ src/client/public/run.script.ts
@@ -2,29 +2,26 @@ import { environment } from '../app/helpers';
 let { config } = PLAYGROUND;
 
 (() => {
-    let ribbons = document.getElementsByClassName('ribbon');
-    for (let i = 0; i < ribbons.length; i++) {
-        let ribbon = ribbons[i] as HTMLElement;
-        switch (environment.current.config.name) {
-            // Utilize Office pallet colors for ribbons
-            case config['insiders'].name:
-                ribbon.textContent = 'Beta';
-                ribbon.style.background = 'rgba(43, 87, 154, 1)';
-                break;
-            case config['edge'].name:
-                ribbon.textContent = 'Alpha';
-                ribbon.style.background = 'rgba(183, 71, 42, 1)';
-                break;
-            case config['local'].name:
-                ribbon.textContent = config['local'].editorUrl;
-                ribbon.style.background = 'rgba(33, 115, 70, 1)';
-                break;
-            default:
-                break;
-        }
+    let ribbon = document.getElementById('ribbon');
+    switch (environment.current.config.name) {
+        // Utilize Office pallet colors for ribbons
+        case config['insiders'].name:
+            ribbon.textContent = 'Beta';
+            ribbon.style.background = 'rgba(43, 87, 154, 1)';
+            break;
+        case config['edge'].name:
+            ribbon.textContent = 'Alpha';
+            ribbon.style.background = 'rgba(183, 71, 42, 1)';
+            break;
+        case config['local'].name:
+            ribbon.textContent = config['local'].editorUrl;
+            ribbon.style.background = 'rgba(33, 115, 70, 1)';
+            break;
+        default:
+            break;
+    }
 
-        if (environment.current.config.name !== config['production'].name) {
-            ribbon.style.visibility = 'visible';
-        }
+    if (environment.current.config.name !== config['production'].name) {
+        ribbon.style.visibility = 'visible';
     }
 })();
