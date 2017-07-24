a695fa15a8d801007be54d02d1c9262d95142749
Shashwat Chaturvedi <t-shac@microsoft.com>
2017-07-24 16:09:43 -0700

    Fix typo

--- config/deploy.js
+++ config/deploy.js
@@ -103,7 +103,7 @@ function deployBuild(url, folder) {
         shell.cd(next_path);
         const start = Date.now();
         if (url === EDITOR_URL) {
-            buildAssetHistory(url);
+            buildAssetHistory(url, folder);
         }
         shell.exec('git init');
         shell.exec('git config --add user.name "Travis CI"');
