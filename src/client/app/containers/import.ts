1f7b7dbe5b80bc545da6bd7fc4c06b9e673f741e
Shashwat Chaturvedi <t-shac@microsoft.com>
2017-08-23 18:12:48 -0700

    Fix small typo

--- src/client/app/containers/import.ts
+++ src/client/app/containers/import.ts
@@ -206,7 +206,7 @@ export class Import {
     }
 
     new() {
-        this._store.dispatch(new Snippet.ImportAction( { mode: Snippet.ImportType.DEFAULT, data: null, isViewMode: false}));
+        this._store.dispatch(new Snippet.ImportAction({ mode: Snippet.ImportType.DEFAULT, data: null, isViewMode: false}));
         this._store.dispatch(new UI.ToggleImportAction(false));
     }
 
