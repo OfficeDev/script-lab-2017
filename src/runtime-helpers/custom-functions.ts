e68274cdf919afb12a0355ad10569755196bdc5b
Zlatkovsky <michael.zlat@gmail.com>
2017-09-26 21:13:10 -0700

    Fix up TSCONFIG files

--- src/runtime-helpers/custom-functions.ts
+++ src/runtime-helpers/custom-functions.ts
@@ -4,71 +4,9 @@
  * necessary because some APIs (such as displayDialogAsync) cannot be used directly within Script Lab.
  */
 module ScriptLab {
-    /** [PREVIEW] Gets an access token on behalf of the user for a particular service
-     * @param definitions: The function definitions, nested inside of an
-     * "Excel.CustomFunctionNamespaceCollection" data structure
+    /** [PREVIEW] Registers a custom functions with Excel (must be done via this helper in order to work correctly in Script Lab)
+     * @param definitions: The function definitions, nested inside of an "Excel.CustomFunctionNamespaceCollection" data structure
      */
-
-    export function registerCustomFunctions() {
-
+    export function registerCustomFunctions(definitions: Excel.CustomFunctionNamespaceCollection) {
     }
-}
-
-interface CustomFunctionNamespaceCollection {
-    [index: string]: CustomFunctionDefinitionCollection;
-}
-interface CustomFunctionDefinitionCollection {
-    [index: string]: CustomFunctionDefinition;
-}
-interface CustomFunctionDefinition {
-    call: Function;
-    description?: string;
-    helpUrl?: string;
-    result: CustomFunctionResult;
-    parameters: Array<CustomFunctionParameter>;
-    options?: CustomFunctionOptions;
-}
-interface CustomFunctionResult {
-    resultType: string;
-    resultDimensionality?: string;
-}
-interface CustomFunctionOptions {
-    batch?: boolean;
-    stream?: boolean;
-}
-/**
- *
- * Custom function parameter declaration.
- *
- * [Api set: ExcelApi 1.7 (PREVIEW)]
- */
-interface CustomFunctionParameter {
-    /**
-     *
-     * Useful description of the parameter.
-     *
-     * [Api set: ExcelApi 1.7 (PREVIEW)]
-     */
-    description: string;
-    /**
-     *
-     * The name of the parameter.
-     *
-     * [Api set: ExcelApi 1.7 (PREVIEW)]
-     */
-    name: string;
-    /**
-     *
-     * The dimensionality of parameter value.
-     *
-     * [Api set: ExcelApi 1.7 (PREVIEW)]
-     */
-    valueDimensionality: string;
-    /**
-     *
-     * The type of parameter value.
-     *
-     * [Api set: ExcelApi 1.7 (PREVIEW)]
-     */
-    valueType: string;
 }
