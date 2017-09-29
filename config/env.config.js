df31d095025862423da2e48b20f50fcb559a7516
Zlatkovsky <michael.zlat@gmail.com>
2017-09-29 13:19:48 -0700

    Re-merge edited external URLs Originally in https://github.com/OfficeDev/script-lab/commit/f6824d1f7228853e7e10b9d5b923b39b94bf7991#diff-0371b9439741639436eb0c486c05c57c

--- config/env.config.js
+++ config/env.config.js
@@ -66,10 +66,10 @@ var safeExternalUrls = {
     code: window.location.origin + '/?mode=${Utilities.host}',
     playground_help: 'https://github.com/OfficeDev/script-lab/blob/master/README.md',
     ask: 'https://stackoverflow.com/questions/tagged/office-js',
-    excel_api: 'https://dev.office.com/docs/add-ins/excel/excel-add-ins-javascript-programming-overview',
+    excel_api: 'https://dev.office.com/reference/add-ins/excel/excel-add-ins-reference-overview',
     word_api: 'https://dev.office.com/reference/add-ins/word/word-add-ins-reference-overview',
-    onenote_api: 'https://dev.office.com/docs/add-ins/onenote/onenote-add-ins-programming-overview',
-    outlook_api: 'https://docs.microsoft.com/en-us/outlook/add-ins/',
+    onenote_api: 'https://dev.office.com/reference/add-ins/onenote/onenote-add-ins-javascript-reference',
+    outlook_api: 'https://docs.microsoft.com/en-us/outlook/add-ins/reference',
     powepoint_api: 'https://dev.office.com/docs/add-ins/powerpoint/powerpoint-add-ins',
     project_api: 'https://dev.office.com/reference/add-ins/shared/projectdocument.projectdocument',
     generic_api: 'https://dev.office.com/reference/add-ins/javascript-api-for-office'
