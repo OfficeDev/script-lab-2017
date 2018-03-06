46391914c7f610e840aadc70e0efae890eaf4d76
Michael Zlatkovsky <Zlatkovsky@users.noreply.github.com>
2018-03-06 13:24:41 -0800

    Update maker.ts

--- src/runtime-helpers/maker.ts
+++ src/runtime-helpers/maker.ts
@@ -45,10 +45,6 @@ module Experimental {
             _onPerfAnalysisReady = params.onPerfAnalysisReady;
         };
 
-        export function setup(clientId: string) {
-            _clientId = clientId;
-        }
-
         export async function tinker(makerCode: (workbook: Excel.Workbook) => any): Promise<any> {
             let runCurtain = parent.window.document.getElementById('curtain');
             runCurtain.style['display'] = 'block';
@@ -62,6 +58,7 @@ module Experimental {
 
                     worker.onerror = (...args) => {
                         console.error(args);
+                        runCurtain.style['display'] = 'none';
                         reject(args);
                     };
 
