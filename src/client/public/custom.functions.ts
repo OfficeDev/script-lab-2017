4c285519279259b8c7f573bb22fdef78c180de6f
REDMOND\ripere <ripere@microsoft.com>
2018-03-27 16:23:12 -0700

    Execution working with office js adhoc 17, static setting js implementation working

--- src/client/public/custom.functions.ts
+++ src/client/public/custom.functions.ts
@@ -5,25 +5,12 @@
 //     environment.initializePartial({ host: 'EXCEL' });
 
 //     (window as any).chocolateFactory = {};
-//     (window as any).chocolateFactory.createChocolate999 = () => Math.random();
+//     (window as any).chocolateFactory.createChocolate = () => Math.random();
 // })();
 
 
-registerCFs();
+(window as any).createChocolate = (num : number) => {
+    return num * 5;
+};
 
-function registerCFs() {
-    (window as any).chocolateFactory = {};
-    (window as any).chocolateFactory.createChocolate999 = function (num: number) {
-        debugger;
-        return num * 5;
-    };
-    (window as any).ScriptLab = {
-        chocolateFactory: (window as any).chocolateFactory
-    };
-}
-
-(window as any).createChocolate999 = (num: number) => {
-    debugger;
-    return 100 * num;
-}
 // };
