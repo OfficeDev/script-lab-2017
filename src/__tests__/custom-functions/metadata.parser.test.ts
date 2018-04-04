8cf3263bd5a8ca5754d7349479b181cebf9ae446
REDMOND\nicobell <nicobell@microsoft.com>
2018-04-04 12:49:35 -0700

    fix

--- src/__tests__/custom-functions/metadata.parser.test.ts
+++ src/__tests__/custom-functions/metadata.parser.test.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs';
 
-import { parseMetadata } from '../../src/server/custom-functions/metadata.parser';
+import { parseMetadata } from '../../server/custom-functions/metadata.parser';
 
 const SAMPLE_DIR = './__tests__/custom-functions/samples';
 
