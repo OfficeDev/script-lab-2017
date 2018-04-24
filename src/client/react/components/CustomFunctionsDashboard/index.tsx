2e5f5f5c421e5692c662c4b53ba8c28c083f0a77
REDMOND\nicobell <nicobell@microsoft.com>
2018-04-24 16:49:55 -0700

    coming soon page for custom functions

--- src/client/react/components/CustomFunctionsDashboard/index.tsx
+++ src/client/react/components/CustomFunctionsDashboard/index.tsx
@@ -1,4 +1,4 @@
-import * as  React from 'react';
+import * as React from 'react';
 import styled from 'styled-components';
 import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
 
@@ -39,105 +39,108 @@ const Content = styled.div`
 // interfaces
 interface ICustomFunctionsDashboardState {
   metadata: {
-    data: object[],
-    isVisible: boolean,
+    data: object[];
+    isVisible: boolean;
   };
   logs: any[];
-};
+}
 
 interface ICustomFunctionsDashboardProps {
   placeholder?: any;
-};
+}
 
-class CustomFunctionsDashboard extends React.Component<ICustomFunctionsDashboardProps, ICustomFunctionsDashboardState> {
+class CustomFunctionsDashboard extends React.Component<
+  ICustomFunctionsDashboardProps,
+  ICustomFunctionsDashboardState
+> {
   constructor(props) {
     super(props);
-    this.state = {metadata: {data, isVisible: false}, logs: []};
+    this.state = { metadata: { data, isVisible: false }, logs: [] };
 
-    setInterval(()=>{
+    setInterval(() => {
       const l = this.state.logs;
       l.push(new Date().toISOString());
-      this.setState({logs: l});
+      this.setState({ logs: l });
     }, 100);
   }
 
   toggleMetadata = () => {
-    this.setState({metadata: {isVisible: !this.state.metadata.isVisible, data: this.state.metadata.data}});
-  }
-
+    this.setState({
+      metadata: {
+        isVisible: !this.state.metadata.isVisible,
+        data: this.state.metadata.data,
+      },
+    });
+  };
 
   render() {
     const { metadata, logs } = this.state;
-    const content =  metadata.isVisible ?
-      <MetadataTree metadata={ metadata.data } /> :
+    const content = metadata.isVisible ? (
+      <MetadataTree metadata={metadata.data} />
+    ) : (
       <Logs logs={logs} />
+    );
     // console.log(content);
-
     return (
       <Container>
         <Title>Custom Functions</Title>
         <ButtonBar>
           <DefaultButton
-              data-automation-id='see-metadata'
-              disabled={ false }
-              checked={ false }
-              text= { metadata.isVisible ? 'Hide' : 'View' }
-              onClick={ this.toggleMetadata }
-            />
+            data-automation-id="see-metadata"
+            disabled={false}
+            checked={false}
+            text={metadata.isVisible ? 'Hide' : 'View'}
+            onClick={this.toggleMetadata}
+          />
           <DefaultButton
-              data-automation-id='see-metadata'
-              disabled={ false }
-              checked={ false }
-              text='Register'
-              primary={ true }
-              onClick={ this.toggleMetadata }
-            />
+            data-automation-id="see-metadata"
+            disabled={false}
+            checked={false}
+            text="Register"
+            primary={true}
+            onClick={this.toggleMetadata}
+          />
         </ButtonBar>
-        <Content>
-          {content}
-        </Content>
-
+        <Content>{content}</Content>
       </Container>
     );
   }
 }
 
 // Test DATA
-const param1 = {name: 'x', status: 'good'}
-const param2 = {name: 'y', status: 'warning'}
-const param3 = {name: 'z', status: 'error', error: 'Some error message!'}
+const param1 = { name: 'x', status: 'good' };
+const param2 = { name: 'y', status: 'warning' };
+const param3 = { name: 'z', status: 'error', error: 'Some error message!' };
 
 const func1 = {
   name: 'foo',
   status: 'good',
-  parameters: [param1, param2]
-}
+  parameters: [param1, param2],
+};
 
 const func2 = {
   name: 'bar',
   status: 'good',
-  parameters: [param1]
-}
+  parameters: [param1],
+};
 
 const func3 = {
   name: 'foobar',
   status: 'error',
-  parameters: [param2, param3]
-}
+  parameters: [param2, param3],
+};
 
 const data = [
   {
     name: 'snippet1',
     status: 'good',
-    functions: [func1, func2, func3]
+    functions: [func1, func2, func3],
   },
   {
     name: 'snippet2',
     status: 'warning',
-    functions: [func1, func3, func1]
-  }
-]
-
-
+    functions: [func1, func3, func1],
+  },
+];
 
 export default CustomFunctionsDashboard;
--- src/client/react/index.tsx
+++ src/client/react/index.tsx
@@ -1,5 +1,37 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
+import ComingSoon from './components/CustomFunctionsDashboard/ComingSoon';
 
-ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
+import { environment } from '../app/helpers';
+
+// Note: Office.initialize is already handled outside in the html page,
+// setting "window.playground_host_ready = true;""
+
+(async () => {
+  environment.initializePartial({ host: 'EXCEL' });
+
+  await new Promise(resolve => {
+    const interval = setInterval(() => {
+      if ((window as any).playground_host_ready) {
+        clearInterval(interval);
+        return resolve();
+      }
+    }, 100);
+  });
+
+  document.getElementById('loading')!.style.display = 'none';
+
+  if (
+    Office &&
+    Office.context &&
+    Office.context.requirements &&
+    Office.context.requirements.isSetSupported('CustomFunctions', 1.1)
+  ) {
+    ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
+  } else {
+    ReactDOM.render(<ComingSoon />, document.getElementById(
+      'root',
+    ) as HTMLElement);
+  }
+})();
