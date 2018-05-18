0459768309fd8e35f9e2a39417da8aa2dc2fc5e2
Zlatkovsky <michael.zlat@gmail.com>
2018-05-17 18:52:02 -0700

    Fix "more info" links in custom functions, to avoid xframe issues

--- src/client/react/components/CustomFunctionsDashboard/Summary/Summary.tsx
+++ src/client/react/components/CustomFunctionsDashboard/Summary/Summary.tsx
@@ -27,7 +27,7 @@ const Summary = ({ metadata }) => {
       const scriptLabToplevelNamespace = 'ScriptLab';
       const name = `=${scriptLabToplevelNamespace}.${snippet.name}.${
         func.name
-      }(${func.parameters.length > 0 ? '…' : ''})`;
+        }(${func.parameters.length > 0 ? '…' : ''})`;
 
       const item: Item = { name, key: name, smallCaps: true };
 
@@ -71,7 +71,7 @@ const Summary = ({ metadata }) => {
           The following custom functions were found in your workspace. These
           functions run async in Script Lab. You can run them faster in sync
           mode with{' '}
-          <a href="https://aka.ms/customfunctions">these instructions</a>.
+          <a href="https://aka.ms/customfunctions" target="_blank">these instructions</a>.
         </p>
       </TopInfo>
       <FunctionsContainer style={{ marginTop: '20px' }}>
