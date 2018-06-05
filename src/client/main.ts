05cef2d600cac6c437732bb9e024149d06900f96
Zlatkovsky <michael.zlat@gmail.com>
2018-06-05 16:33:14 -0700

    Fix issue with Script Lab not working on MSI RTM version

--- src/client/main.ts
+++ src/client/main.ts
@@ -6,14 +6,22 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { Authenticator, UI } from '@microsoft/office-js-helpers';
+import { Authenticator, UI, Utilities, PlatformType } from '@microsoft/office-js-helpers';
 import { StoreModule, Store } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
 
 import { SERVICE_PROVIDERS, MonacoService } from './app/services';
 import { PIPES } from './app/pipes';
-import { EXCEPTION_PROVIDER, applyTheme, AI, storage, environment } from './app/helpers';
+import {
+  EXCEPTION_PROVIDER,
+  applyTheme,
+  AI,
+  storage,
+  environment,
+  isInsideOfficeApp,
+  PlaygroundError,
+} from './app/helpers';
 import { Strings } from './app/strings';
 import { COMPONENT_DECLARATIONS } from './components';
 import { AppComponent, EditorMode, ViewMode, ViewModeError } from './app/containers';
@@ -58,6 +66,20 @@ let imports = [
 
     await applyTheme(environment.current.host);
 
+    if (isInsideOfficeApp() && Utilities.platform === PlatformType.PC) {
+      if (isO16orHigher()) {
+        // For Office 2016 MSI, need to have a build that supports the "GetHostInfo" API.
+        // Otherwise, the code will never run, because switching to the runner domain will lose the host info.
+        try {
+          (window.external as any).GetHostInfo();
+        } catch (e) {
+          throw new PlaygroundError(
+            `Your Office version is missing important updates, that Script Lab can't run without. Please install the latest Office updates from https://docs.microsoft.com/en-us/officeupdates/office-updates-msi`
+          );
+        }
+      }
+    }
+
     if (!Authenticator.isAuthDialog(environment.current.host === 'TEAMS')) {
       AI.trackEvent(`[Perf] Playground ready`);
       await platformBrowserDynamic().bootstrapModule(AppModule);
@@ -65,11 +87,34 @@ let imports = [
     }
   } catch (e) {
     $('.ms-progress-component__sub-title')
-      .text(strings.HtmlPageStrings.errorInitializingScriptLab)
+      .text(
+        e instanceof PlaygroundError
+          ? e.message
+          : strings.HtmlPageStrings.errorInitializingScriptLab
+      )
       .css('cursor', 'pointer')
       .click(() => UI.notify(e));
     $('.ms-progress-component__footer').hide();
     AI.trackException(e, 'Playground Initialization');
+  }
+
+  // Helpers
+  function isO16orHigher(): boolean {
+    const hasVersion =
+      Office &&
+      Office.context &&
+      Office.context.diagnostics &&
+      Office.context.diagnostics.version;
+    if (hasVersion) {
+      const versionString = Office.context.diagnostics.version;
+      let num = Number.parseInt(versionString.substr(0, versionString.indexOf('.')));
+      return num >= 16;
+    }
+
+    // The only hosts that don't support Office.context.diagnostics.version are the 2016 hosts that
+    //     still use the non-updated "16.00" files (by contrast, 15.XX files do support it)
+    // So it's actually a giveaway that they *are* O16.
+    return true;
   }
 })();
 
