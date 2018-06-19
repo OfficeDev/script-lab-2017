919b412b02651ad3ec4397de333de2f794ea25c5
Michael Zlatkovsky <zlatkovsky@users.noreply.github.com>
2018-06-19 13:56:44 -0700

    Fix manifest placeholders to be actual Custom Function paths/entries

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -78,5 +78,10 @@ module.exports = () =>
         version: true,
         warnings: true,
       },
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+        "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+      }
     },
   });
---
+++ src/client/assets/custom-functions/functions.js
@@ -0,0 +1,4 @@
+// Intentionally empty, will instead use runtime-declared JS
+function ADD42(a, b) {
+    return a + b + 42;
+}
