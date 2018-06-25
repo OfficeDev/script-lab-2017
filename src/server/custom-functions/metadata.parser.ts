1d812b855939b1733013f62c95314bd0daccb0ed
nico-bellante <35939126+nico-bellante@users.noreply.github.com>
2018-06-25 10:24:46 -0700

    Add streaming support for Custom Functions (#837)
    * Add streaming support
    
    * addressed code review feedback
    
    * added two more tests

--- src/server/custom-functions/metadata.parser.ts
+++ src/server/custom-functions/metadata.parser.ts
@@ -10,7 +10,7 @@ const TYPE_MAPPINGS = {
   [ts.SyntaxKind.BooleanKeyword]: 'boolean',
 };
 
-const CUSTOM_FUNCTION_OPTIONS_KEYS = ['stream', 'volatile'];
+const CUSTOM_FUNCTION_OPTIONS_KEYS = [];
 
 const CUSTOM_FUNCTION_DEFAULT_OPTIONS: ICustomFunctionOptions = {
   sync: true,
@@ -57,15 +57,23 @@ function traverseAST(sourceFile: ts.SourceFile): ICFFunctionMetadata[] {
           if (isCF) {
             const jsDocParamInfo = getJSDocParams(func);
 
-            const parameters = func.parameters.map((p: ts.ParameterDeclaration) => {
-              const name = (p.name as ts.Identifier).text;
+            const [lastParameter] = func.parameters.slice(-1);
+            const isStreamingFunction = isLastParameterStreaming(lastParameter);
+            const paramsToParse = isStreamingFunction
+              ? func.parameters.slice(0, func.parameters.length - 1)
+              : func.parameters.slice(0, func.parameters.length);
 
-              return {
-                name,
-                ...(jsDocParamInfo[name] ? { description: jsDocParamInfo[name] } : {}),
-                ...getDimAndTypeHelper(p.type),
-              };
-            });
+            const parameters = paramsToParse
+              .map((p: ts.ParameterDeclaration) => {
+                const name = (p.name as ts.Identifier).text;
+
+                return {
+                  name,
+                  ...(jsDocParamInfo[name] ? { description: jsDocParamInfo[name] } : {}),
+                  ...getDimAndTypeHelper(p.type),
+                };
+              })
+              .filter(meta => meta);
 
             let description;
             if ((func as any).jsDoc) {
@@ -77,7 +85,22 @@ function traverseAST(sourceFile: ts.SourceFile): ICFFunctionMetadata[] {
               error?: string;
               type: CustomFunctionsSupportedTypes;
             };
-            if (func.type) {
+            if (isStreamingFunction) {
+              const lastParameterType = lastParameter.type as ts.TypeReferenceNode;
+              if (
+                !lastParameterType.typeArguments ||
+                lastParameterType.typeArguments.length !== 1
+              ) {
+                result = {
+                  error:
+                    'One and only one argument should be specified to IStreamingCustomFunctionHandler',
+                  dimensionality: 'invalid',
+                  type: 'invalid',
+                };
+              } else {
+                result = getDimAndTypeHelper(lastParameterType.typeArguments[0]);
+              }
+            } else if (func.type) {
               if (
                 func.type.kind === ts.SyntaxKind.TypeReference &&
                 (func.type as ts.TypeReferenceNode).typeName.getText() === 'Promise' &&
@@ -105,6 +128,11 @@ function traverseAST(sourceFile: ts.SourceFile): ICFFunctionMetadata[] {
               func.modifiers[0].kind === ts.SyntaxKind.AsyncKeyword
             ) {
               options.sync = true;
+            }
+
+            if (isStreamingFunction) {
+              options.stream = true;
+              options.cancelable = true;
             }
 
             const metadataItem = {
@@ -139,6 +167,16 @@ function traverseAST(sourceFile: ts.SourceFile): ICFFunctionMetadata[] {
 }
 
 // helpers
+
+function isLastParameterStreaming(param?: ts.ParameterDeclaration): boolean {
+  const isTypeReferenceNode = param && ts.isTypeReferenceNode(param.type);
+  if (!isTypeReferenceNode) {
+    return false;
+  }
+
+  const typeRef = param.type as ts.TypeReferenceNode;
+  return typeRef.typeName.getText() === 'IStreamingCustomFunctionHandler';
+}
 
 function getDimAndTypeHelper(
   t: ts.TypeNode
@@ -184,16 +222,16 @@ function parseCustomFunctionOptions(
 function getJSDocParams(node: ts.Node): { [key: string]: string } {
   const jsDocParamInfo = {};
 
-  ts
-    .getAllJSDocTagsOfKind(node, ts.SyntaxKind.JSDocParameterTag)
-    .forEach((tag: ts.JSDocParameterTag) => {
+  ts.getAllJSDocTagsOfKind(node, ts.SyntaxKind.JSDocParameterTag).forEach(
+    (tag: ts.JSDocParameterTag) => {
       const comment = (tag.comment.startsWith('-')
         ? tag.comment.slice(1)
         : tag.comment
       ).trim();
 
       jsDocParamInfo[(tag as ts.JSDocPropertyLikeTag).name.getFullText()] = comment;
-    });
+    }
+  );
 
   return jsDocParamInfo;
 }
